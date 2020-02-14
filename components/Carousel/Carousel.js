345776953572be7deadee97f8e828838f57618c2
Natalia Beckstead <toosmiravilla@gmail.com>
2020-02-14 11:20:27 -0700

    stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,49 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carouselCreator() {
+  const images = ["./assets/carousel/mountains.jpeg", "./assets/carousel/computer.jpeg", "./assets/carousel/trees.jpeg", "./assets/carousel/turntable.jpeg"];
+  let i = 0;
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const img = document.createElement('img');
+  const rightButton = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  leftButton.textContent = " < ";
+  rightButton.textContent = " > ";
+  img.src = images[i];
+  img.style.display = 'inline-block';
+  img.style.zIndex = -1;
+
+  leftButton.addEventListener('click', () => {
+    i -= 1;
+    if (i < 0) {
+      i = 3;
+    }
+    img.src = images[i];
+    return i;
+  });
+
+  rightButton.addEventListener('click', () => {
+    i += 1;
+    if (i > 3) {
+      i = 0;
+    }
+    img.src = images[i];
+    return i;
+  });
+
+  carousel.append(leftButton);
+  carousel.append(img);
+  carousel.append(rightButton);
+
+  return carousel;
+}
+
+const carouselHolder = document.querySelector('.carousel-container');
+carouselHolder.appendChild(carouselCreator());
